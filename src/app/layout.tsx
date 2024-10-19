2707260bf75c31cb6bca851bafd8fe6bae8cf044
cccwon2 <cccwon2@naver.com>
2024-10-19 17:03:46 +0900

    [Feat] Header 컴포넌트 추가 및 login, signup 페이지 초기 버전 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,15 @@
 import { Metadata } from "next";
-<<<<<<< HEAD
-import "./globals.css";
-import Header from "@/components/Header";
-=======
 import "../styles/globals.css";
 import { Toaster } from "react-hot-toast";
->>>>>>> develop
+import Header from "@/components/Header";
 
 export const metadata: Metadata = {
   title: {
     template: "%s | Taskify",
-    default: "새로운 일정관리 | Taskify"
+    default: "새로운 일정관리 | Taskify",
   },
   description: "새로운 일정관리 | Taskify",
-  icons: { icon: "/icons/favicon.ico", shortcut: "/icons/favicon.ico"}
+  icons: { icon: "/icons/favicon.ico", shortcut: "/icons/favicon.ico" },
 };
 
 export default function RootLayout({
@@ -23,14 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-<<<<<<< HEAD
-      <body>
+      <body className="font-pretendard text-black03 min-h-screen">
         <Header />
-=======
-      <body className="min-h-screen font-pretendard text-black03">
->>>>>>> develop
         {children}
-        <Toaster toastOptions={{ success: {style: { fontSize: "14px" }}, error: {style: { fontSize: "14px" }} }} />
+        <Toaster toastOptions={{ success: { style: { fontSize: "14px" } }, error: { style: { fontSize: "14px" } } }} />
       </body>
     </html>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,50 @@
+"use client";
+
 import Link from "next/link";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Header() {
+  const [isLargeScreen, setIsLargeScreen] = useState(false);
+
+  useEffect(() => {
+    // 화면 크기를 감지하여 md 이상인지 확인하는 함수
+    const handleResize = () => {
+      setIsLargeScreen(window.innerWidth >= 768);
+    };
+
+    // 초기 실행 및 리스너 추가
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    // 컴포넌트 언마운트 시 리스너 제거
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
-    <header className="bg-gray-800 p-4 text-white">
-      <nav>
+    <header className="h-[60px] border-b border-gray-300 bg-white px-[24px] md:h-[70px] md:px-[40px] xl:px-[70px]">
+      <nav className="ml-[20px] flex h-full items-center justify-between gap-[17px] pr-[24px] md:gap-[15px]">
+        {/* 로고와 Taskify 텍스트를 감싸는 컨테이너 */}
+        <div className="flex items-center space-x-2">
+          <div className="flex-shrink-0">
+            {isLargeScreen ? (
+              <Image src="/images/header/logo_md.svg" alt="로고" width={110} height={34} />
+            ) : (
+              <Image src="/images/header/logo.svg" alt="로고" width={24} height={30} />
+            )}
+          </div>
+        </div>
+        {/* 로그인/회원가입 메뉴 */}
         <ul className="flex space-x-4">
           <li>
-            <Link href="/auth/login">로그인</Link>
+            <Link className="text-base font-normal md:text-lg" href="/auth/login">
+              로그인
+            </Link>
           </li>
           <li>
-            <Link href="/auth/signup">회원가입</Link>
+            <Link className="text-base font-normal md:text-lg" href="/auth/signup">
+              회원가입
+            </Link>
           </li>
         </ul>
       </nav>
