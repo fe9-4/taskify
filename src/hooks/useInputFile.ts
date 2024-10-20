c7c3e5a4c8ce5dec451047c369175c045f6efcb0
JIHYEMIN <hyemeeeny@gmail.com>
2024-10-21 00:10:43 +0900

    [Feat] InputFile 커스텀 훅 추가

---
+++ src/hooks/useInputFile.ts
@@ -0,0 +1,23 @@
+import { ChangeEvent, useRef, useState } from "react";
+
+export const useInputFile = () => {
+  const imageRef = useRef<HTMLImageElement>(null);
+  const [preview, setPreview] = useState<string | null>(null);
+
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.currentTarget.files?.[0];
+
+    if (file) {
+      // FileReader 객체 생성
+      const reader = new FileReader();
+      // 파일 읽는 작업 시작
+      reader.readAsDataURL(file);
+      // 파일 읽는 작업 완료 후 호출될 onloadend 메서드 정의
+      reader.onloadend = () => {
+        setPreview(reader.result as string);
+      };
+    }
+  };
+
+  return { imageRef, preview, handleFileChange };
+};
