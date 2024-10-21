43a54c8b03bfd7cf67b52a01baada9d8a677344b
JIHYEMIN <hyemeeeny@gmail.com>
2024-10-21 15:53:08 +0900

    [Refactor] 이미지 hover 수정

--- src/components/InputFile.tsx
+++ src/components/InputFile.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { FaPlus as PlusImage } from "react-icons/fa6";
 import { InputFileProps } from "@/types/formType";
 import { useInputFile } from "@/hooks/useInputFile";
+import ImageEdit from "../../public/icons/image_edit.svg";
 
 const InputFile = ({ label, size }: InputFileProps) => {
   const { imageRef, preview, handleFileChange } = useInputFile();
@@ -13,8 +14,8 @@ const InputFile = ({ label, size }: InputFileProps) => {
       {label && <p className="text-lg font-normal text-black03">{label}</p>}
 
       <div className={`relative flex ${inputFileSize.size[size]} cursor-pointer overflow-hidden rounded-md`}>
-        <label htmlFor="file" className="grid h-full w-full cursor-pointer place-items-center gap-[24px] bg-slate-200">
-          <PlusImage size="17px" color="#5534DA" />
+        <label htmlFor="file" className="bg-#F5F5F5 grid size-full cursor-pointer place-items-center gap-[24px]">
+          <PlusImage className="size-[17px] text-violet01" />
         </label>
 
         <input
@@ -22,11 +23,14 @@ const InputFile = ({ label, size }: InputFileProps) => {
           type="file"
           onChange={handleFileChange}
           accept="image/*"
-          className="absolute left-0 top-0 z-10 h-full w-full cursor-pointer opacity-0"
+          className="absolute left-0 top-0 z-[1] size-full cursor-pointer opacity-0"
         />
 
         {preview && (
           <>
+            <span className="absolute left-0 top-0 z-[2] flex size-full items-center justify-center bg-black bg-opacity-60 opacity-0 transition-opacity duration-300 hover:opacity-100">
+              <Image src={ImageEdit} alt="이미지 수정" className="size-[30px]" />
+            </span>
             <Image ref={imageRef} src={preview} fill alt="이미지 미리보기" className="z-0 object-cover" />
           </>
         )}
@@ -37,8 +41,8 @@ const InputFile = ({ label, size }: InputFileProps) => {
 
 export const inputFileSize = {
   size: {
-    todo: "w-[76px] h-[76px]",
-    profile: "w-[100px] h-[100px] md:w-[182px] md:h-[182px]",
+    todo: "size-[76px]",
+    profile: "size-[100px] md:size-[182px]",
   },
 };
 
