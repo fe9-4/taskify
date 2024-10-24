13cf21afdaef0dc82f3ad97e423155e72b570651
JIHYEMIN <hyemeeeny@gmail.com>
2024-10-25 02:14:09 +0900

    [Refactor] 할 일 생성 api

--- src/app/api/cards/route.ts
+++ src/app/api/cards/route.ts
@@ -1,8 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
 import { cookies } from "next/headers";
-import axios, { AxiosError } from "axios";
+import axios from "axios";
 import apiClient from "@/app/api/apiClient";
-import { CardProps } from "@/types/cardType";
+
+// YYYY-MM-DD HH:MM 형식으로 날짜를 포맷팅하는 함수
+function formatDateTime(date: Date): string {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  return `${year}-${month}-${day} ${hours}:${minutes}`;
+}
 
 // 할 일 생성
 export const POST = async (request: NextRequest) => {
@@ -14,20 +23,46 @@ export const POST = async (request: NextRequest) => {
   }
 
   try {
-    const formData: CardProps = await request.json();
-    const response = await apiClient.post("/cards", formData, {
+    const body = await request.json();
+    const { assigneeUserId, dashboardId, columnId, title, description, dueDate, tags, imageUrl } = body;
+
+    // 태그를 JSON 문자열에서 배열로 변환
+    const parsedTags = JSON.parse(tags);
+
+    // dueDate를 YYYY-MM-DD HH:MM 형식으로 변환
+    const formattedDueDate = dueDate ? formatDateTime(new Date(dueDate)) : null;
+
+    // 새로운 카드 객체 생성
+    const newCard = {
+      assigneeUserId: Number(assigneeUserId),
+      dashboardId: Number(dashboardId),
+      columnId: Number(columnId),
+      title,
+      description,
+      dueDate: formattedDueDate,
+      tags: parsedTags,
+      imageUrl,
+    };
+
+    // API 클라이언트를 사용하여 서버에 카드 생성 요청
+    const response = await apiClient.post("/cards", newCard, {
       headers: {
         Authorization: `Bearer ${accessToken}`,
         "Content-Type": "multipart/form-data",
       },
     });
 
-    return NextResponse.json({ user: response.data }, { status: 200 });
+    return NextResponse.json(response.data, { status: 201 });
   } catch (error) {
     if (axios.isAxiosError(error)) {
-      console.error("할 일 생성 오류", error);
-      return new NextResponse("할 일 생성 실패", { status: error.status });
+      console.error("카드 생성 중 오류 발생: ", error);
+
+      if (error.response) {
+        return NextResponse.json({ message: error.response.data.message }, { status: error.status });
+      }
     }
+
+    return NextResponse.json({ error: "카드 생성 중 오류가 발생했습니다." }, { status: 500 });
   }
 };
 
@@ -41,7 +76,7 @@ export const PUT = async (request: NextRequest) => {
   }
 
   try {
-    const formData: CardProps = await request.json();
+    const formData = await request.json();
     const response = await apiClient.put("/cards", formData, {
       headers: {
         Authorization: `Bearer ${accessToken}`,
