6ba4fb0ffa30ee853a7c73f1961ba4ca10d436ad
hongggy <hongggy@gmail.com>
2024-10-24 16:45:42 +0900

    [Feat] route.ts에서 params적용

--- src/app/api/dashboard/list/route.ts
+++ src/app/api/dashboard/list/route.ts
@@ -1,14 +1,16 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { cookies } from "next/headers";
 import apiClient from "@/app/api/apiClient";
 
-export async function GET() {
+export async function GET(request: NextRequest) {
   const cookieStore = cookies();
   const accessToken = cookieStore.get("accessToken")?.value;
 
   // 모바일 10개, 태블릿 15개, 데스크탑 15개
-  const query = { page: 1, size: 15 };
-  const { page, size } = query;
+  const { searchParams } = new URL(request.url);
+  const page = searchParams.get("page") || 1;
+  const size = searchParams.get("size") || 10;
+
   const requestUrl = `/9-4/dashboards?navigationMethod=pagination&page=${page}&size=${size}`;
   if (!accessToken) {
     return NextResponse.json({ user: null }, { status: 401 });
--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -9,21 +9,26 @@ import { useEffect, useState } from "react";
 import { useAuth } from "@/hooks/useAuth";
 
 const Sidebar = () => {
-  const onClickPrev = () => {};
-  const onClickNext = () => {};
+  const [page, setPage] = useState(1);
+  const [size, setSize] = useState(10); // 기본 값으로 10
+  const [totalCount, setTotalCount] = useState<number>();
   const pathname = usePathname();
 
   const [dashboardList, setDashboardList] = useState<ItemType[]>([]);
   const { user } = useAuth();
-  const fetchDashboardList = async () => {
+  const fetchDashboardList = async (page: number, size: number) => {
     if (user) {
       try {
-        const res = await fetch("/api/dashboard/list", { method: "GET", credentials: "include" });
+        const res = await fetch(`/api/dashboard/list?page=${page}&size=${size}`, {
+          method: "GET",
+          credentials: "include",
+        });
         if (!res.ok) {
           throw new Error(`Failed to fetch data : ${res.status}`);
         }
         const data = await res.json();
         setDashboardList(data.user ? data.user.dashboards : []);
+        setTotalCount(data.user.totalCount);
       } catch (err) {
         console.error(err);
       }
@@ -31,10 +36,12 @@ const Sidebar = () => {
   };
 
   useEffect(() => {
-    fetchDashboardList();
+    fetchDashboardList(page, size);
     console.log(dashboardList);
   }, [user]);
 
+  const onClickPrev = () => {};
+  const onClickNext = () => {};
   if (pathname === "/" || pathname === "/login" || pathname === "/signup") return null;
   return (
     <aside className="fixed left-0 top-0 z-50 flex h-screen w-[67px] flex-col gap-[39px] border-r border-gray03 bg-white px-[13px] py-5 md:w-[160px] md:gap-[57px] xl:w-[300px] xl:pl-2 xl:pr-3">
