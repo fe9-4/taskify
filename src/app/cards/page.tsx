5d43760655dff92de302c948f7e6d61b8cfeb88d
JIHYEMIN <hyemeeeny@gmail.com>
2024-10-24 13:54:37 +0900

    [Chore] cards 수정

--- src/app/cards/page.tsx
+++ src/app/cards/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import CreateCard from "@/components/cards/CreateCard";
+import UpdateCard from "@/components/cards/UpdateCard";
 
 const Cards = () => {
   return (
     <>
       <CreateCard />
+      <UpdateCard />
     </>
   );
 };
