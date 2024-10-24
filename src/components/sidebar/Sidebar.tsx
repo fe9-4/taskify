b2f4f4eeb82eb558a2042dce15dfb4d851e62fd9
hongggy <hongggy@gmail.com>
2024-10-24 13:58:25 +0900

    [Style] zindex 수정

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -8,7 +8,7 @@ const Sidebar = () => {
   const pathname = usePathname();
   if (pathname === "/" || pathname === "/login" || pathname === "/signup") return null;
   return (
-    <aside className="fixed left-0 top-0 z-30 flex h-screen w-[67px] flex-col gap-[39px] border-r border-gray03 bg-white px-[13px] py-5 md:w-[160px] md:gap-[57px] xl:w-[300px] xl:pl-2 xl:pr-3">
+    <aside className="fixed left-0 top-0 z-50 flex h-screen w-[67px] flex-col gap-[39px] border-r border-gray03 bg-white px-[13px] py-5 md:w-[160px] md:gap-[57px] xl:w-[300px] xl:pl-2 xl:pr-3">
       <Logo />
       <div className="w-full">
         <Button />
