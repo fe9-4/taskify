5a2393dbe3befb7add5b5f8f429857d805c5c170
hongggy <hongggy@gmail.com>
2024-10-24 12:53:22 +0900

    [Style] 대시보드 리스트 타이틀 렌더링 수정

--- src/components/sidebar/DashboardItem.tsx
+++ src/components/sidebar/DashboardItem.tsx
@@ -7,16 +7,13 @@ import { FaCircle, FaCrown } from "react-icons/fa6";
 const DashboardItem = ({ item }: { item: ItemType }) => {
   const { color, title, createdByMe } = item;
   let length = 5;
-  let shortTitle;
-  if (title.length > length) {
-    shortTitle = title.slice(0, 5) + "...";
-  }
+  const shortTitle = title.length > length ? title.slice(0, 5) + "..." : title;
   return (
     <div className="flex h-10 w-10 items-center justify-center md:w-full md:py-2 md:pl-[10px] md:pr-0 xl:p-3">
       <div className="flex w-full justify-center md:w-full md:items-center md:justify-start md:gap-4">
         <FaCircle fill={color} width={8} height={8} className="size-2 flex-shrink-0" />
         <div className="flex items-center gap-[6px] md:gap-1">
-          <div className="hidden whitespace-nowrap font-medium text-gray01 md:block md:text-lg xl:hidden">
+          <div className="hidden font-medium text-gray01 md:block md:whitespace-nowrap md:text-lg xl:hidden">
             {shortTitle}
           </div>
           <div className="hidden xl:block xl:text-xl">{title}</div>
