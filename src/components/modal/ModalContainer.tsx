a1a2f1017d682763f2f955fa0e3ff7574740f9d7
BugO <kco796@naver.com>
2024-10-24 16:53:20 +0900

    [Chore] 전역 변수 불러오는 코드 수정

--- src/components/modal/ModalContainer.tsx
+++ src/components/modal/ModalContainer.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import Modal from "react-modal";
-import { useAtom } from "jotai";
+import { useAtom, useAtomValue } from "jotai";
 import { AlertModalAtom, AlertModalTextAtom, CreateCardAtom, CreateDashboardAtom } from "@/store/modalAtom";
 import CreateDashboard from "@/components/modal/CreateDashboard";
 import ModalContent from "./ModalContent";
@@ -16,7 +16,7 @@ const ModalContainer = () => {
   const [isCreateCardOpen, setIsCreateCardOpen] = useAtom(CreateCardAtom);
   const [isAlertOpen, setIsAlertOpen] = useAtom(AlertModalAtom);
 
-  const [AlertText] = useAtom(AlertModalTextAtom);
+  const alertText = useAtomValue(AlertModalTextAtom);
 
   return (
     <>
@@ -27,7 +27,7 @@ const ModalContainer = () => {
         <CreateCard />
       </ModalContent>
       <ModalContent isOpen={isAlertOpen} onRequestClose={() => setIsAlertOpen(false)}>
-        <AlertModal text={AlertText} />
+        <AlertModal text={alertText} />
       </ModalContent>
     </>
   );
