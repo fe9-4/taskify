56934ca1f8f66f436756095400e4a0b68380a207
JIHYEMIN <hyemeeeny@gmail.com>
2024-10-25 18:38:54 +0900

    [Chore] 할 일 카드 수정 api 삭제

--- src/app/api/cards/route.ts
+++ src/app/api/cards/route.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 import apiClient from "@/app/api/apiClient";
 import { formatDateTime } from "@/utils/dateFormat";
 
-// 할 일 생성
+// 할 일 카드 생성
 export const POST = async (request: NextRequest) => {
   const cookieStore = cookies();
   const accessToken = cookieStore.get("accessToken")?.value;
@@ -53,36 +53,5 @@ export const POST = async (request: NextRequest) => {
     }
 
     return NextResponse.json({ error: "할 일 카드 생성 중 오류가 발생했습니다." }, { status: 500 });
-  }
-};
-
-// 할 일 카드 수정
-export const PUT = async (request: NextRequest) => {
-  const cookieStore = cookies();
-  const accessToken = cookieStore.get("accessToken")?.value;
-
-  if (!accessToken) {
-    return NextResponse.json({ error: "인증되지 않은 사용자" }, { status: 401 });
-  }
-
-  try {
-    const formData = await request.json();
-    const response = await apiClient.put("/cards", formData, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
-
-    return NextResponse.json({ user: response.data }, { status: 201 });
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      console.error("할 일 카드 수정 중 오류 발생: ", error);
-
-      if (error.response) {
-        return NextResponse.json({ message: error.response.data.message }, { status: error.status });
-      }
-    }
-
-    return NextResponse.json("할 일 카드 수정 중 오류가 발생했습니다.", { status: 500 });
   }
 };
