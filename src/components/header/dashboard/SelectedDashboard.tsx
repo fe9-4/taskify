f77ed2a9788330fe9d263a05d7dc0864c444cb9d
cccwon2 <cccwon2@naver.com>
2024-10-26 17:06:22 +0900

    [Chore] SelectedDashboard 에 React.FC 제거

--- src/components/header/dashboard/SelectedDashboard.tsx
+++ src/components/header/dashboard/SelectedDashboard.tsx
@@ -1,12 +1,11 @@
-import React from "react";
 import { Dashboard } from "@/zodSchema/dashboardSchema";
 
 interface SelectedDashboardProps {
   dashboard: Dashboard | null;
 }
 
-export const SelectedDashboard: React.FC<SelectedDashboardProps> = ({ dashboard }) => {
-  // dashboards가 undefined이거나 빈 배열인 경우 처리
+export const SelectedDashboard = ({ dashboard }: SelectedDashboardProps) => {
+  // 대시보드가 없는 경우 처리
   if (!dashboard) {
     return <div>대시보드가 없습니다.</div>;
   }
