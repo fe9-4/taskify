bc7602933cff3e0ae56b7e3326d82495f4add53b
byeolee1221 <zicsso1221@gmail.com>
2024-10-26 12:28:25 +0900

    [Chore] cards 폴더 내부 할 일 수정 api 삭제 (위치 변경하여 중복 가능성)

--- src/app/api/cards/route.ts
+++ src/app/api/cards/route.ts
@@ -1,7 +1,7 @@
-import { NextRequest, NextResponse } from "next/server";
-import { cookies } from "next/headers";
-import axios, { AxiosError } from "axios";
+import axios from "axios";
 import apiClient from "@/app/api/apiClient";
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
 import { CardProps } from "@/types/cardType";
 
 // 할 일 생성
@@ -27,33 +27,6 @@ export const POST = async (request: NextRequest) => {
     if (axios.isAxiosError(error)) {
       console.error("할 일 생성 오류", error);
       return new NextResponse("할 일 생성 실패", { status: error.status });
-    }
-  }
-};
-
-// 할 일 수정
-export const PUT = async (request: NextRequest) => {
-  const cookieStore = cookies();
-  const accessToken = cookieStore.get("accessToken")?.value;
-
-  if (!accessToken) {
-    return NextResponse.json({ error: "인증되지 않은 사용자" }, { status: 401 });
-  }
-
-  try {
-    const formData: CardProps = await request.json();
-    const response = await apiClient.put("/cards", formData, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-        "Content-Type": "multipart/form-data",
-      },
-    });
-
-    return NextResponse.json({ user: response.data }, { status: 200 });
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      console.error("할 일 수정 오류", error);
-      return new NextResponse("할 일 수정 실패", { status: error.status });
     }
   }
 };
