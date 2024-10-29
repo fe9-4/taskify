43043ea4901fc9329d4ce19a073bde8da961927f
hongggy <hongggy@gmail.com>
2024-10-29 11:01:41 +0900

    [Feat] 수정 창에서 기존 정보 보여주기

--- src/app/dashboard/[dashboardId]/edit/components/EditDashboard.tsx
+++ src/app/dashboard/[dashboardId]/edit/components/EditDashboard.tsx
@@ -2,7 +2,7 @@
 
 import { ActiveBtn } from "@/components/button/ButtonComponents";
 import InputItem from "@/components/input/InputItem";
-import { useForm } from "react-hook-form";
+import { FieldValues, useForm } from "react-hook-form";
 import SelectColorChip from "@/components/chip/SelectColorChip";
 import axios, { AxiosError } from "axios";
 import toast from "react-hot-toast";
@@ -14,24 +14,18 @@ const EditDashboard = ({ dashboardId }: { dashboardId: number }) => {
     register,
     handleSubmit,
     watch,
+    reset,
     formState: { isValid },
-  } = useForm({ mode: "onChange" });
+  } = useForm<FieldValues>({ mode: "onChange" });
 
-  const [dashboardInfo, setDashboardInfo] = useState<DashboardInfoType>({
-    id: 0,
-    title: "TITLE",
-    color: "#000000",
-    createdAt: "2024-01-01T19:54:48.459Z",
-    updatedAt: "2024-01-01T19:19:34.157Z",
-    userId: 0,
-    createdByMe: false,
-  });
-  const { title, color, createdByMe } = dashboardInfo;
+  const [dashboardInfo, setDashboardInfo] = useState<DashboardInfoType | null>(null);
+
   // 대시보드 정보 요청 - 대시보드 수정하고 새로운 정보 가져오기
   const fetchDashboardInfo = useCallback(async () => {
     try {
       const res = await axios.get(`/api/dashboards/${dashboardId}`);
       setDashboardInfo(res.data);
+      reset({ title: dashboardInfo?.title, color: dashboardInfo?.color });
     } catch (err) {
       const error = err as AxiosError;
       console.error(error.message);
@@ -42,12 +36,26 @@ const EditDashboard = ({ dashboardId }: { dashboardId: number }) => {
     fetchDashboardInfo();
   }, [fetchDashboardInfo]);
 
+  useEffect(() => {
+    if (dashboardInfo) {
+      reset({
+        title: dashboardInfo.title,
+        color: dashboardInfo.color,
+      });
+    }
+  }, [dashboardInfo, reset]);
+
   // 대시보드 수정 api 요청
   const updateDashboard = async (value: ValueType) => {
     try {
       const res = await axios.put(`/api/dashboards/${dashboardId}`, value);
       const data = res.data;
-      setDashboardInfo((prev) => ({ ...prev, title: data.title, color: data.color }));
+      setDashboardInfo((prev) => {
+        if (prev) {
+          return { ...prev, title: data.title, color: data.color };
+        }
+        return prev;
+      });
       toast.success("대시보드 정보가 수정되었습니다");
     } catch (err) {
       const error = err as AxiosError;
@@ -56,8 +64,6 @@ const EditDashboard = ({ dashboardId }: { dashboardId: number }) => {
     }
   };
 
-  //  사이드바랑 헤더도 같이 변경되어야함
-
   const onSubmit = () => {
     const formData = watch() as ValueType;
     updateDashboard(formData);
@@ -65,13 +71,18 @@ const EditDashboard = ({ dashboardId }: { dashboardId: number }) => {
 
   return (
     <div className="w-full rounded-2xl bg-white px-4 py-5 md:px-7 md:py-8">
-      <h2 className="mb-6 text-2xl font-bold md:text-3xl">{title || "TITLE"}</h2>
-      <InputItem id="title" {...register("title", { required: true })} label="대시보드 이름" type="text" />
+      <h2 className="mb-6 text-2xl font-bold md:text-3xl">{dashboardInfo?.title || "TITLE"}</h2>
+      <InputItem
+        id="title"
+        {...register("title", { required: "대시보드 이름을 입력해주세요" })}
+        label="대시보드 이름"
+        type="text"
+      />
       <div className="mb-8 mt-4 md:mb-10">
         <SelectColorChip register={register} watch={watch} />
       </div>
       <div className="flex h-[54px] w-full gap-2">
-        <ActiveBtn disabled={!isValid && !createdByMe} onClick={handleSubmit(onSubmit)}>
+        <ActiveBtn disabled={!isValid && !dashboardInfo?.createdByMe} onClick={handleSubmit(onSubmit)}>
           변경
         </ActiveBtn>
       </div>
