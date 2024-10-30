a566fab77fba3112063a2ffe1a29df4a20b46185
JIHYEMIN <hyemeeeny@gmail.com>
2024-10-30 23:15:55 +0900

    [Chore] 댓글 타입 수정

--- src/types/commentType.ts
+++ src/types/commentType.ts
@@ -20,14 +20,3 @@ export interface CommentListProps {
   cardId: number;
   columnId: number;
 }
-
-// export interface CommentItemProps {
-//   comment: CommentProps;
-//   setComments Dispatch<SetStateAction<CommentProps[]>>;
-//   // editCommentId: number | null;
-//   // editContent: string;
-//   // handleEdit: (commentId: number | null, currentContent: string) => void;
-//   // handleDelete: (commentId: number) => void;
-//   // handleEditChange: ChangeEventHandler;
-//   // handleSave: (commentId: number) => void;
-// }
