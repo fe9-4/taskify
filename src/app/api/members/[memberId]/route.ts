28d57aa1ba0f7e74eeff60958e765e3ff50a2fa5
hongggy <hongggy@gmail.com>
2024-10-30 09:57:16 +0900

    [Style] edit 페이지 반응형 디자인

--- src/app/api/members/[memberId]/route.ts
+++ src/app/api/members/[memberId]/route.ts
@@ -1,37 +1,35 @@
 import { NextRequest, NextResponse } from "next/server";
 import { cookies } from "next/headers";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { apiClient } from "@/app/api/apiClient";
 
-export async function DELETE(request: NextRequest, { params }: { params: { memberId: number } }) {
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { memberId: number } }
+): Promise<NextResponse> {
   const accessToken = cookies().get("accessToken")?.value;
   const { memberId } = params;
 
-  if (!accessToken) {
+  if (accessToken) {
+    try {
+      await apiClient.delete(`/members/${memberId}`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      return new NextResponse(null, { status: 204 });
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.error("할 일 카드 삭제 중 오류 발생: ", error);
+
+        if (error.response) {
+          return NextResponse.json({ message: error.response.data.message }, { status: error.status });
+        }
+      }
+    }
+  } else {
     return NextResponse.json({ message: "인증되지 않은 요청입니다." }, { status: 401 });
   }
 
-  try {
-    await apiClient.delete(`/members/${memberId}`, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
-    console.log("멤버 삭제 성공");
-
-    return new NextResponse(null, { status: 204 });
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      console.error("멤버 삭제 중 오류 발생: ", error);
-
-      if (error.response) {
-        return NextResponse.json(
-          { message: error.response.data.message || "오류 발생" },
-          { status: error.response.status }
-        );
-      }
-    }
-
-    return NextResponse.json({ error: "멤버 삭제 중 오류가 발생했습니다." }, { status: 500 });
-  }
+  return NextResponse.json({ message: "대시보드 멤버 삭제에 실패했습니다." }, { status: 400 });
 }
--- src/app/dashboard/[dashboardId]/edit/components/DashboardMemberList.tsx
+++ src/app/dashboard/[dashboardId]/edit/components/DashboardMemberList.tsx
@@ -28,6 +28,7 @@ const DashboardMemberList = ({ dashboardId }: { dashboardId: number }) => {
     const deleteMember = async (id: number) => {
       try {
         const response = await axios.delete(`/api/members/${id}`);
+        console.log(response);
         if (response?.status === 204) {
           toast.success(`멤버 ${nickname}가 삭제되었습니다`);
           setMemberList(members.members.filter((member) => member.userId !== id));
