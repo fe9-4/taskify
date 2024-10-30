617bdea0f91b7a66f5fe92894d8a382b67e5292b
JIHYEMIN <hyemeeeny@gmail.com>
2024-10-30 23:03:47 +0900

    [Refactor] 무한 스크롤 수정

--- src/components/modal/comments/CommentList.tsx
+++ src/components/modal/comments/CommentList.tsx
@@ -1,123 +1,68 @@
-import { useCallback, useEffect, useRef, useState } from "react";
-import { useAtom } from "jotai";
-import { useToggleModal } from "@/hooks/useToggleModal";
-import { AlertModalConfirmAtom, AlertModalTextAtom } from "@/store/modalAtom";
+import { useCallback, useEffect, useState } from "react";
 import { CommentListProps, CommentProps } from "@/types/commentType";
 import axios from "axios";
 import toast from "react-hot-toast";
 import CommentItem from "./CommentItem";
 import CreateComment from "./CreateComment";
+import useIntersectionObserver from "@/hooks/useObserver";
 
 const CommentList = ({ cardId, columnId }: CommentListProps) => {
-  const toggleModal = useToggleModal();
-  const [, setAlertText] = useAtom(AlertModalTextAtom);
-  const [, setOnConfirm] = useAtom(AlertModalConfirmAtom);
-
   const [comments, setComments] = useState<CommentProps[]>([]);
-  const size = 5;
-  const [cursorId, setCursorId] = useState(0);
-
-  const observeRef = useRef<IntersectionObserver | null>(null);
-  const loadingRef = useRef<HTMLDivElement | null>(null);
-  const [hasMore, setHasMore] = useState(true);
+  const [cursorId, setCursorId] = useState<number | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const size = 10;
 
   const getComments = useCallback(async () => {
-    if (!hasMore) return;
+    if (isLoading) return;
+    setIsLoading(true);
 
     try {
       const response = await axios.get(
         `/api/comments?size=${size}${cursorId ? `&cursorId=${cursorId}` : ""}&cardId=${cardId}`
       );
       const newCommentList = response.data.comments;
+      const nextCursorId = response.data.cursorId;
 
-      console.log("커서", response.data.cursorId);
-
-      setComments((prev) => {
-        // const uniqueComments = newCommentList.filter(
-        //   (newComment: any) => !prevComments.some((prevComment) => prevComment.id === newComment.id)
-        // );
-        // const data = new Set(uniqueComments);
-        // return [...prevComments, ...uniqueComments];
-        const existingId = new Set(prev.map((comment) => comment.id));
-        const filteredNewCardList = newCommentList.filter((comment: any) => !existingId.has(comment.id));
-
-        if (filteredNewCardList.length === 0 || filteredNewCardList.length < size) {
-          setHasMore(false);
-        }
-
-        return [...prev, ...filteredNewCardList];
-      });
-
-      console.log(comments);
-
-      if (newCommentList.length < size) {
-        setHasMore(false); // 더 이상 불러올 댓글이 없을 때 false로 설정
+      if (newCommentList.length > 0) {
+        setComments((prev) => [...prev, ...newCommentList]);
+        setCursorId(nextCursorId);
+      } else {
+        setCursorId(null);
       }
     } catch (error) {
       if (axios.isAxiosError(error)) {
         const errorMessage = error.response?.data?.message || "오류가 발생했습니다.";
         toast.error(errorMessage);
       }
+    } finally {
+      setIsLoading(false);
     }
-  }, [cardId, hasMore, size]);
-
-  // 댓글 무한스크롤
-  useEffect(() => {
-    getComments();
-
-    observeRef.current = new IntersectionObserver((entries) => {
-      const lastCommentItem = entries[0];
-
-      if (lastCommentItem.isIntersecting && hasMore) {
-        getComments();
-      }
-    });
-
-    const currentLoadingRef = loadingRef.current;
-
-    if (currentLoadingRef) observeRef.current.observe(currentLoadingRef);
-
-    return () => {
-      if (currentLoadingRef) observeRef.current?.unobserve(currentLoadingRef);
-    };
-  }, [hasMore, size, getComments]);
+  }, [cardId, isLoading, size, cursorId]);
 
   useEffect(() => {
     getComments();
-    setHasMore(true);
-  }, [getComments]);
+  }, []);
 
-  // const handleDelete = (commentId: number) => {
-  //   toggleModal("deleteModal", true);
-  //   setAlertText("정말 삭제하시겠습니까?");
-  //   setOnConfirm(() => {
-  //     ondelete(commentId);
-  //     toggleModal("deleteModal", false);
-  //   });
-  // };
+  const setObserveTarget = useIntersectionObserver(() => {
+    if (!isLoading) {
+      getComments();
+    }
+  });
 
   return (
     <>
       <CreateComment cardId={cardId} columnId={columnId} setComments={setComments} />
-      <div className="mt-4 h-[100vh] overflow-y-scroll">
+      <div className="mt-4 overflow-y-scroll rounded-2xl [&::-webkit-scrollbar]:hidden">
         {comments.length > 0 ? (
-          comments.map((comment, i) => (
-            <div key={comment.id}>
-              <CommentItem comment={comment} setComments={setComments} />
-              {i === comments.length - 1 && <div ref={loadingRef} className="h-[1px]" />}
-            </div>
-          ))
+          comments.map((comment) => <CommentItem key={comment.id} comment={comment} setComments={setComments} />)
         ) : (
           <p className="mb-3 flex items-center justify-center text-center text-xs font-medium text-gray02 md:mb-2 md:text-lg">
             등록된 댓글이 없습니다.
           </p>
         )}
       </div>
-      {/* {hasMore && (
-        <div ref={loadingRef} className="h-10">
-          로딩 중...
-        </div>
-      )} */}
+      <div ref={setObserveTarget} style={{ height: "1px" }} />
+      {isLoading && <p className="text-gray02">Loading...</p>}
     </>
   );
 };
---
+++ src/hooks/useObserver.ts
@@ -0,0 +1,32 @@
+import { useEffect, useRef, useState } from "react";
+
+const useIntersectionObserver = (callback: () => void) => {
+  const [observeTarget, setObserveTarget] = useState<HTMLDivElement | null>(null);
+
+  const observer = useRef(
+    new IntersectionObserver(
+      ([entry]) => {
+        if (!entry.isIntersecting) return;
+        callback();
+      },
+      { threshold: 1 }
+    )
+  );
+
+  useEffect(() => {
+    const currentTarget = observeTarget;
+    const currentObserver = observer.current;
+
+    if (currentTarget) currentObserver.observe(currentTarget);
+
+    return () => {
+      if (currentTarget) {
+        currentObserver.unobserve(currentTarget);
+      }
+    };
+  }, [observeTarget]);
+
+  return setObserveTarget;
+};
+
+export default useIntersectionObserver;
