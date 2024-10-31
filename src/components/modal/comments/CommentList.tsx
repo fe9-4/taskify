7d17fb64c6d9ab588d87ee961f730704eb87a450
JIHYEMIN <hyemeeeny@gmail.com>
2024-11-01 00:23:17 +0900

    [Refactor] 댓글 무한스크롤 수정

--- src/components/modal/comments/CommentList.tsx
+++ src/components/modal/comments/CommentList.tsx
@@ -1,19 +1,17 @@
-import { Dispatch, useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { CommentListProps, CommentProps } from "@/types/commentType";
 import axios from "axios";
 import toast from "react-hot-toast";
 import CommentItem from "./CommentItem";
 import CreateComment from "./CreateComment";
 import useIntersectionObserver from "@/hooks/useObserver";
-import { SetStateAction } from "jotai";
 
 const CommentList = ({ cardId, columnId }: CommentListProps) => {
   const [comments, setComments] = useState<CommentProps[]>([]);
   const [cursorId, setCursorId] = useState<number | null>(null);
   const [isLoading, setIsLoading] = useState(false);
-  // const [observe, setObserve] = useState(false);
-  const [hasMore, setHasMore] = useState(true); // 더 불러올 데이터가 있는지 확인
-  const size = 3;
+  const [hasMore, setHasMore] = useState(true);
+  const size = 10;
 
   const getComments = useCallback(async () => {
     if (isLoading || !hasMore) return;
@@ -38,42 +36,47 @@ const CommentList = ({ cardId, columnId }: CommentListProps) => {
         });
 
         setCursorId(nextCursorId || null);
-        setHasMore(Boolean(nextCursorId)); // 다음 페이지가 없을 경우 false로 설정
+        setHasMore(Boolean(nextCursorId));
       }
     } catch (error) {
       if (axios.isAxiosError(error)) {
-        const errorMessage = error.response?.data?.message || "오류가 발생했습니다.";
-        toast.error(errorMessage);
+        toast.error("댓글 조회에 실패하였습니다.");
       }
     } finally {
       setIsLoading(false);
     }
   }, [cardId, cursorId, size, isLoading, hasMore]);
 
-  // 컴포넌트 첫 마운트 시 한 번만 호출
-  useEffect(() => {
-    getComments();
-  }, []); // 빈 배열로 설정하여 컴포넌트 마운트 시 한 번만 호출
-
-  // 무한 스크롤에 필요한 관찰 대상 설정
-  const setObserveTarget: Dispatch<SetStateAction<any>> = useIntersectionObserver(() => {
+  const targetRef = useIntersectionObserver(() => {
     if (!isLoading && hasMore) {
       getComments();
     }
   });
+
+  useEffect(() => {
+    if (comments.length === 0 && hasMore) {
+      getComments();
+    }
+  }, []);
 
   return (
     <>
       <CreateComment cardId={cardId} columnId={columnId} setComments={setComments} />
       <div className="mt-4 overflow-y-scroll rounded-2xl [&::-webkit-scrollbar]:hidden">
         {comments.length > 0 ? (
-          comments.map((comment) => <CommentItem key={comment.id} comment={comment} setComments={setComments} />)
+          comments.map((comment, index) => (
+            <CommentItem
+              key={comment.id}
+              comment={comment}
+              setComments={setComments}
+              ref={index === comments.length - 1 ? targetRef : null}
+            />
+          ))
         ) : (
           <p className="mb-3 flex items-center justify-center text-center text-xs font-medium text-gray02 md:mb-2 md:text-lg">
             등록된 댓글이 없습니다.
           </p>
         )}
-        <div ref={setObserveTarget} className="h-1" />
       </div>
       {isLoading && <p className="text-xs text-gray02 md:text-base">Loading...</p>}
     </>
--- src/hooks/useObserver.ts
+++ src/hooks/useObserver.ts
@@ -1,17 +1,15 @@
-import { MutableRefObject, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const useIntersectionObserver = (callback: () => void) => {
   const [observeTarget, setObserveTarget] = useState<HTMLElement | null>(null);
   const observer = useRef<IntersectionObserver | null>(null);
   const savedCallback = useRef(callback);
 
-  // callback이 변경될 때마다 savedCallback 업데이트
   useEffect(() => {
     savedCallback.current = callback;
   }, [callback]);
 
   useEffect(() => {
-    // IntersectionObserver가 이미 없을 때만 새로 생성
     if (!observer.current) {
       observer.current = new IntersectionObserver(
         ([entry]) => {
@@ -23,17 +21,10 @@ const useIntersectionObserver = (callback: () => void) => {
       );
     }
 
-    // 기존의 타겟이 있을 경우, 관찰 해제
-    if (observeTarget) {
-      observer.current.unobserve(observeTarget);
-    }
-
-    // 새로운 타겟 관찰 시작
     if (observeTarget) {
       observer.current.observe(observeTarget);
     }
 
-    // 컴포넌트 언마운트 시 IntersectionObserver 해제
     return () => {
       if (observer.current && observeTarget) {
         observer.current.unobserve(observeTarget);
