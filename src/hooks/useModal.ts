1c6524c67506977e3a8ba56010c9ba636d09c3a4
BugO <kco796@naver.com>
2024-10-31 16:13:28 +0900

    [Chore] 커스텀훅 useCallback 적용

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -1,22 +1,29 @@
 import { DeleteModalStateAtom, initiModalState, ModalStateAtom } from "@/store/modalAtom";
 import { useSetAtom } from "jotai";
+import { useCallback } from "react";
 
-// 모달 상태를 토글하는 함수
+// 모달 상태를 토글 hook
 export const useToggleModal = () => {
   const setModalState = useSetAtom(ModalStateAtom);
 
-  return (modalName: keyof typeof initiModalState, isOpen: boolean) => {
-    setModalState((prev) => ({ ...prev, [modalName]: isOpen }));
-  };
+  return useCallback(
+    (modalName: keyof typeof initiModalState, isOpen: boolean) => {
+      setModalState((prev) => ({ ...prev, [modalName]: isOpen }));
+    },
+    [setModalState]
+  );
 };
 
-// 삭제 모달 상태 관리
+// 삭제 모달 상태 관리 hook
 export const useDeleteModal = () => {
   const setDeleteModalState = useSetAtom(DeleteModalStateAtom);
   const setModalState = useSetAtom(ModalStateAtom);
 
-  return (confirm: () => void, title: string) => {
-    setDeleteModalState({ title, confirm });
-    setModalState((prev) => ({ ...prev, deleteModal: true }));
-  };
+  return useCallback(
+    (confirm: () => void, title: string) => {
+      setDeleteModalState({ title, confirm });
+      setModalState((prev) => ({ ...prev, deleteModal: true }));
+    },
+    [setDeleteModalState, setModalState]
+  );
 };
