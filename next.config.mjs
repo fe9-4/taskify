ef2d064163dd0e95379857b65ee6a105e7a48766
cccwon2 <cccwon2@naver.com>
2024-11-01 10:10:35 +0900

    [Fix] 서버를 재시작 했을 때 쿠키가 남아있는 유저가 자동 로그인이 안되는 현상

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.m?js$/,
+      type: "javascript/auto",
+      resolve: {
+        fullySpecified: false,
+      },
+    });
+    return config;
+  },
   images: {
     remotePatterns: [
       {
